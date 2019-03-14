32de4dd4fef8632e2202399b9be805fd6b4f9073
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-03-14 16:39:52 +0200

    add middelware to routes

--- app/Post.php
+++ app/Post.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-        protected $fillable = [
+    protected $fillable = [
         'title',
         'description',
         'user_id',
     ];
-     
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/posts' , 'PostController@index' )->name('posts.index');
-Route::get('/posts/create', 'PostController@create')->name('posts.create');
-Route::post('/posts','PostController@store')->name('posts.store');
-Route::get('/posts/{post}/edit','PostController@edit')->name('posts.edit');
-Route::put('/posts/{post}','PostController@update')->name('posts.update');
-Route::delete('/posts/{post}','PostController@destroy')->name('posts.destroy');
-Route::get('/posts/{post}' , 'PostController@show' )->name('posts.show');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/posts', 'PostController@index')->name('posts.index');
+    Route::get('/posts/create', 'PostController@create')->name('posts.create');
+    Route::post('/posts', 'PostController@store')->name('posts.store');
+    Route::get('/posts/{post}/edit', 'PostController@edit')->name('posts.edit');
+    Route::put('/posts/{post}', 'PostController@update')->name('posts.update');
+    Route::delete('/posts/{post}', 'PostController@destroy')->name('posts.destroy');
+    Route::get('/posts/{post}', 'PostController@show')->name('posts.show');
+});
 
 
 
